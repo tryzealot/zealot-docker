c2bfed75ae13e8d61ee39087b288c4a3a5653aac
icyleaf <icyleaf.cn@gmail.com>
2024-03-14 14:29:00 +0800

    fix: volumes 判断的小问题

--- scripts/configure-volumes.sh
+++ scripts/configure-volumes.sh
@@ -66,7 +66,7 @@ choose_volumes () {
 }
 
 
-VOLUMES_EXISTS=$(grep -cE "^(\s+)zealot\-(\w+):" $DOCKER_COMPOSE_FILE || echo 0)
+VOLUMES_EXISTS=$(grep -qcE "^(\s+)zealot\-(\w+):" $DOCKER_COMPOSE_FILE || echo 0)
 if [ "$VOLUMES_EXISTS" -eq 4 ]; then
   echo "Volumes already exists, skipped"
 else
