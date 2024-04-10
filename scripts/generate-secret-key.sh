23761891f1e77979e883e23336dceb8e81f1f88a
icyleaf <icyleaf.cn@gmail.com>
2024-04-10 18:31:08 +0800

    fix: generate 128-bit chars for secret token
    #17

--- scripts/generate-secret-key.sh
+++ scripts/generate-secret-key.sh
@@ -3,10 +3,10 @@ echo "${_group}Generating secret key ..."
 HAS_SECRET_TOKEN=$(grep 'SECRET_TOKEN' $ENV_FILE | awk '{split($0,a,"="); print a[2]}')
 
 if [ -z "$HAS_SECRET_TOKEN" ]; then
-  SECRET_TOKEN=$(export LC_ALL=C; head /dev/urandom | tr -dc "a-z0-9@#%^&*(-_=+)" | head -c 50 | sed -e 's/[\/&]/\\&/g')
+  SECRET_TOKEN=$(export LC_ALL=C; head /dev/urandom | tr -dc "a-z0-9" | head -c 128 | sed -e 's/[\/&]/\\&/g')
   sed -i -e 's/^SECRET_TOKEN=.*$/SECRET_TOKEN='"'$SECRET_TOKEN'"'/' $ENV_FILE
   clean_sed_temp_file $ENV_FILE
-  echo "Secret key written to .env"
+  echo "Secret key written to .env: \`${SECRET_TOKEN}\`"
 else
   echo "Secret key had been write, skipped"
 fi
