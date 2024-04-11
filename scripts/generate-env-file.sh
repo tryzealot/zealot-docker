2d2ef0e11cd5e59e5dc83a39d52fee7a62f2c3c3
icyleaf <icyleaf.cn@gmail.com>
2024-04-11 12:35:17 +0800

    fix: .env file missing REDIS_URL

--- scripts/generate-env-file.sh
+++ scripts/generate-env-file.sh
@@ -7,7 +7,7 @@ else
   cp -n "${EXAMPLE_ENV_FILE}" "${ENV_FILE}"
 fi
 
-if [ "${REDIS_ENABLED_LEGACY:-0}" == 1 && grep 'REDIS_URL=' $ENV_FILE ]; then
+if [ "${REDIS_ENABLED_LEGACY:-0}" == 1 ] && ! grep -q "REDIS_URL" $ENV_FILE; then
   echo "" >> $ENV_FILE
   echo "# The full Redis URL for the Redis cache." >> $ENV_FILE
   echo "REDIS_URL=redis://redis:6379/0" >> $ENV_FILE
