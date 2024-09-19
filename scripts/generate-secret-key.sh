92097ead6fa897e9d48ca4d7c176fd90a3df7eea
icyleaf <icyleaf.cn@gmail.com>
2024-09-20 00:02:14 +0800

    feat: add SECRET_KEY_BASE instead of SECRET_TOKEN

--- scripts/generate-secret-key.sh
+++ scripts/generate-secret-key.sh
@@ -1,12 +1,13 @@
 echo "${_group}Generating secret key ..."
 
-HAS_SECRET_TOKEN=$(grep 'SECRET_TOKEN' $ENV_FILE | awk '{split($0,a,"="); print a[2]}')
+HAS_SECRET_TOKEN=$(grep -E 'SECRET_KEY_BASE|SECRET_TOKEN' $ENV_FILE | awk '{split($0,a,"="); print a[2]}')
 
 if [ -z "$HAS_SECRET_TOKEN" ]; then
-  SECRET_TOKEN=$(export LC_ALL=C; head /dev/urandom | tr -dc "a-z0-9" | head -c 128 | sed -e 's/[\/&]/\\&/g')
-  sed -i -e 's/^SECRET_TOKEN=.*$/SECRET_TOKEN='"'$SECRET_TOKEN'"'/' $ENV_FILE
+  token=$(export LC_ALL=C; head /dev/urandom | tr -dc "a-z0-9" | head -c 128 | sed -e 's/[\/&]/\\&/g')
+  sed -i -e 's/^SECRET_TOKEN=.*$/SECRET_TOKEN='"'$token'"'/' $ENV_FILE
+  sed -i -e 's/^SECRET_KEY_BASE=.*$/SECRET_KEY_BASE='"'$token'"'/' $ENV_FILE
   clean_sed_temp_file $ENV_FILE
-  echo "Secret key written to .env: \`${SECRET_TOKEN}\`"
+  echo "Secret key written to .env: \`${token}\`"
 else
   echo "Secret key had been write, skipped"
 fi
